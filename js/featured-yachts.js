b86ac8c0ff7661c6c3eee73ac99dd68a03f9f76c
captainclarkhaley <clark@mvroam.com>
2026-06-08 08:06:06 -0400

    Featured yacht: update position 3

--- js/featured-yachts.js
+++ js/featured-yachts.js
@@ -43,7 +43,7 @@ const featuredYachts = [
         description: "Because of their renowned build quality and Bruce Farr’s exceptional design, the Southern Wind 72 is equally at home crossing oceans, effortlessly clicking off 200‑mile days or competing in long‑distance racing!",
         link: "contact.html",
         linkText: "Inquire",
-        pdf: "documents/yachts/featured/1991-southern-wind-bruce-farr-fast-72.pdf",
+        pdf: "documents/yachts/featured/1991-Southern_Wind-Bruce_Farr_Fast_72.pdf",
         kuula: "<iframe src=\"https://kuula.co/share/collection/7MFLp?logo=-1&info=1&fs=1&vr=0&gyro=0&thumbs=1&keys=0\" width=\"100%\" height=\"640\" frameborder=\"0\" allow=\"xr-spatial-tracking; gyroscope; accelerometer\" allowfullscreen ></iframe>",
         page: "yachts/fortunato.html"
     },
