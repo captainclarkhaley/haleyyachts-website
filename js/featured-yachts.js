d1286cb44c9bc29f74771f45a09b256d7af04a52
captainclarkhaley <clark@mvroam.com>
2026-07-13 21:45:35 -0400

    Featured yacht: update position 5

--- js/featured-yachts.js
+++ js/featured-yachts.js
@@ -69,9 +69,9 @@ const featuredYachts = [
         description: "This exceptional 2023 Riviera 64 Sport Motor Yacht is a highly optioned example that has been thoughtfully equipped for luxury cruising, entertaining, and extended adventures. Twin MAN 1150 hp engines, Seakeeper 18. Call for full details.",
         link: "contact.html",
         linkText: "Inquire",
-        pdf: "documents/yachts/featured/2023-Riviera-64_Sports_Motor_Yacht.pdf",
+        pdf: "documents/yachts/featured/2026-HCB-56_SUENOS_GIX.pdf",
         kuula: "",
-        youtube: "",
+        youtube: "asIe1cVN4IE",
         page: ""
     },
     {
